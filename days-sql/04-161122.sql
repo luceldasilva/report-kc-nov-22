d90e4b73f88ef8c71fc5ca6c77319d23a4eda1c8
Lucel Da Silva <luceldasilva@yahoo.com>
2022-11-18 17:08:38 -0300

    actualizado hasta ele 16/11

---
+++ days-sql/04-161122.sql
@@ -0,0 +1,208 @@
+-- 17/11/22
+insert into kcnov22 (usuario, deck, skill, ndmax, zerotg, zephra, bryan, xenoblur) 
+values 
+	('9379', 'Red-Eyes', 'Ruleta de Ojos Rojos', '14/11/2022', true, true, true, false),
+	('8812', 'Code Talker', 'Codificador Hablador Vivo', '14/11/2022', true, false, true, false),
+	('8050', 'Railway', 'Tren Imparable', '14/11/2022', true, false, true, false),
+	('9212', 'Railway', 'Tren Imparable', '14/11/2022', true, false, true, false),
+	('2007', 'Fish Shark', 'El alzamiento de Shark', '14/11/2022', true, false, false, false),
+	('0349', 'Metaphys', 'Con Valor', '14/11/2022', true, true, true, false),
+	('1048', 'Burning Abyss Infernoid', 'Mente de la Plana', '14/11/2022', true, true, false, true),
+	('4001', 'Gouki', 'Acceso Tormenta', '14/11/2022', true, false, false, false),
+	('9761', 'Salamangreat', 'Vinculaciones', '14/11/2022', true, false, false, false),
+	('0487', 'Salamangreat', 'Alzarse desde el Valle de las Llamas', '14/11/2022', true, true, true, false),
+	('3717', 'Salamangreat', 'Alzarse desde el Valle de las Llamas', '14/11/2022', true, false, false, false),
+	('1188', 'Fish Armor', 'Gemelos Unidos', '14/11/2022', true, true, false, false),
+	('3401', 'Crystrons', 'Barrera de Luz', '14/11/2022', true, true, false, false),
+	('0178', 'Salamangreat', 'Alzarse desde el Valle de las Llamas', '14/11/2022', true, false, false, false),
+	('9451', 'Fish Shark', 'El alzamiento de Shark', '14/11/2022', true, false, false, false),
+	('7899', 'Satellarknights', 'Habilidad no asignada', '14/11/2022', true, false, false, false),
+	('3902', 'Buster Blader', 'Robar sentido Nivel bajo', '14/11/2022', true, false, false, false),
+	('9540', 'Railway', 'Tren Imparable', '14/11/2022', true, true, true, false),
+	('1831', 'Salamangreat', 'Alzarse desde el Valle de las Llamas', '14/11/2022', true, false, false, false),
+	('2356', 'Odd-Eyes', 'Xiangke y Xiangsheng', '14/11/2022', true, true, false, false),
+	('4774', 'Gouki', 'Calentamiento Extra', '14/11/2022', true, false, false, true),
+	('7579', 'Salamangreat', 'Alzarse desde el Valle de las Llamas', '14/11/2022', true, false, false, false),
+	('0869', 'Gimmick Puppet', 'Marioneta Renunciada', '14/11/2022', true, false, false, false),
+	('6439', 'Mermail Atlantean', 'Territorio de Tiburones', '15/11/2022', true, false, false, false),
+	('3447', 'Salamangreat', 'Alzarse desde el Valle de las Llamas', '15/11/2022', true, false, false, false),
+	('7624', 'Fish Shark', 'El alzamiento de Shark', '15/11/2022', true, false, false, false),
+	('3367', 'Triamids', 'Barrera de Luz', '15/11/2022', true, true, false, false),
+	('3882', 'Odd-Eyes', 'Xiangke y Xiangsheng', '15/11/2022', true, false, false, false),
+	('4194', 'Block Dragon', 'Robo de Magia Trampa', '15/11/2022', true, false, true, false),
+	('2807', 'Galaxy-Eyes', 'Galaxia XYZ', '15/11/2022', true, false, false, false),
+	('9337', 'Salamangreat', 'Alzarse desde el Valle de las Llamas', '15/11/2022', true, false, false, false),
+	('9361', 'Code Talker', 'Codificador Hablador Vivo', '15/11/2022', true, false, false, false),
+	('5747', 'Mekk-Knights', 'Mecanismos Antigüos', '15/11/2022', true, false, false, false),
+	('0148', 'Yosenjus', 'Robo del Destino', '15/11/2022', true, false, false, false),
+	('1411', 'Fire Fist', 'Paliza', '15/11/2022', true, false, true, false),
+	('5780', 'Mermail Atlantean', 'Territorio de Tiburones', '15/11/2022', true, false, false, false),
+	('5918', 'Valkyrie', 'Maestro de Rituales II', '15/11/2022', true, false, false, false),
+	('5462', 'Salamangreat', 'Vinculaciones', '15/11/2022', true, true, false, false),
+	('2566', 'Salamangreat', 'Vinculaciones', '15/11/2022', true, false, false, false),
+	('8364', 'Mermail Atlantean', 'Territorio de Tiburones', '15/11/2022', true, true, false, false),
+	('4058', 'Blue-Eyes', 'Dragones Definitivos', '15/11/2022', true, false, false, false),
+	('3836', 'Burning Abyss Phantom Knights', 'Mente de la Plana', '16/11/2022', true, false, false, false),
+	('1894', 'Phantom Knights', 'Estrategia Fantasma', '16/11/2022', true, true, false, false),
+	('5814', 'Rose Dragon', 'Conjuro de Rosas', '16/11/2022', true, false, false, false),
+	('5494', 'Dark Magician', 'Robo del Destino', '16/11/2022', true, false, false, false),
+	('1078', 'Dark Magician', 'As en la Manga', '16/11/2022', true, false, true, false),
+	('0885', 'Mermail Atlantean', 'Territorio de Tiburones', '16/11/2022', true, false, false, false),
+	('0306', 'Salamangreat', 'Alzarse desde el Valle de las Llamas', '16/11/2022', true, false, true, false),
+	('2805', 'Salamangreat', 'Alzarse desde el Valle de las Llamas', '16/11/2022', true, false, false, false),
+	('6152', 'Odd-Eyes', 'Xiangke y Xiangsheng', '16/11/2022', true, false, false, false),
+	('8123', 'Fluffal', 'Revelación del Castillo Supremo', '16/11/2022', true, false, false, false),
+	('0786', 'Infernity', 'Fantasma Hueco', '16/11/2022', true, false, false, false),
+	('8735', 'Yosenjus', 'Robo del Destino', '16/11/2022', true, false, false, false),
+	('2918', 'Infernoid', 'Compensación', '16/11/2022', true, false, false, false),
+	('0397', 'Mermail Atlantean', 'Territorio de Tiburones', '16/11/2022', true, false, false, false),
+	('2687', 'Synchrons', 'Camino de la Estrella Fugaz', '16/11/2022', true, true, true, true),
+	('7049', 'Salamangreat', 'Alzarse desde el Valle de las Llamas', '16/11/2022', true, false, false, false),
+	('3672', 'Railway', 'Tren Imparable', '16/11/2022', true, false, true, false),
+	('1639', 'Rose Dragon', 'Conjuro de Rosas', '14/11/2022', false, true, false, false),
+	('8504', 'Fish Shark', 'El alzamiento de Shark', '14/11/2022', false, true, false, false),
+	('3859', 'Six Samurais', 'ZEXAL Evolución XYZ', '14/11/2022', false, true, false, false),
+	('0015', 'Railway', 'Tren Imparable', '14/11/2022', false, true, false, false),
+	('1277', 'Galaxy-Eyes', 'Galaxia XYZ', '14/11/2022', false, true, false, false),
+	('4601', 'D/D/D', 'El Mando del Rey de la Perdición', '14/11/2022', false, true, false, false),
+	('4618', 'Metalfoes', 'Cambio de Péndulo Básico', '14/11/2022', false, true, true, false),
+	('5592', 'Salamangreat', 'Alzarse desde el Valle de las Llamas', '14/11/2022', false, true, false, false),
+	('6989', 'Salamangreat', 'Vinculaciones', '14/11/2022', false, true, false, false),
+	('0928', 'D/D/D', 'El Mando del Rey de la Perdición', '14/11/2022', false, true, false, false),
+	('7629', 'Code Talker', 'Codificador Hablador Vivo', '14/11/2022', false, true, false, false),
+	('8304', 'Galaxy-Eyes', 'Galaxia XYZ', '14/11/2022', false, true, false, false),
+	('0419', 'Ultra Athletes', 'Robar sentido Nivel bajo', '15/11/2022', false, true, true, false),
+	('2475', 'Odd-Eyes', 'Xiangke y Xiangsheng', '15/11/2022', false, true, true, false),
+	('9826', 'Salamangreat', 'Alzarse desde el Valle de las Llamas', '15/11/2022', false, true, false, false),
+	('1702', 'Phantom Knights', 'La Orden de los Caballeros Fantasma', '15/11/2022', false, true, false, false),
+	('9922', 'Satellarknights', 'Robo del Destino', '15/11/2022', false, true, false, true),
+	('9428', 'Code Talker', 'Codificador Hablador Vivo', '15/11/2022', false, true, false, false),
+	('8468', 'Dark Magician', 'Robar sentido Oscuridad', '15/11/2022', false, true, false, false),
+	('0022', 'Meklord', 'Refinamiento Meklord', '15/11/2022', false, true, false, false),
+	('1981', 'Galaxy-Eyes', 'Galaxia XYZ', '15/11/2022', false, true, false, false),
+	('4698', 'Salamangreat', 'Alzarse desde el Valle de las Llamas', '15/11/2022', false, true, false, false),
+	('7448', 'Fossil', 'Guerreros Fósiles', '15/11/2022', false, true, true, false),
+	('7178', 'Salamangreat', 'Alzarse desde el Valle de las Llamas', '15/11/2022', false, true, false, false),
+	('3680', 'World Chalice', 'Acceso Tormenta', '15/11/2022', false, true, false, false),
+	('5900', 'Abyss Actor', 'Tumbas Selladas', '15/11/2022', false, true, false, false),
+	('7396', 'Tech Genus', 'Control de Vuelo', '15/11/2022', false, true, false, false),
+	('1807', 'Necroz', 'Maestro de Rituales', '16/11/2022', false, true, false, false),
+	('0857', 'Railway', 'Tren Imparable', '16/11/2022', false, true, false, false),
+	('1769', 'Infernity', 'Fantasma Hueco', '16/11/2022', false, true, false, false),
+	('5236', 'Mekk-Knights', 'Robo del Destino', '16/11/2022', false, true, false, false),
+	('3466', 'Mermail Atlantean', 'Territorio de Tiburones', '16/11/2022', false, true, false, false),
+	('1441', 'Mermail Atlantean', 'Territorio de Tiburones', '16/11/2022', false, true, true, false),
+	('1900', 'Resonators', 'Resonancia Demoníaca', '16/11/2022', false, true, false, false),
+	('4548', 'Salamangreat', 'Alzarse desde el Valle de las Llamas', '16/11/2022', false, true, false, false),
+	('7681', 'Gouki', 'El Evento Principal Gouki', '16/11/2022', false, true, false, false),
+	('4254', 'Ice Barrier', 'Territorio de Tiburones', '16/11/2022', false, true, false, false),
+	('4811', 'Wounded Heros', 'Héroe Herido', '14/11/2022', false, false, true, false),
+	('6631', 'D/D/D', 'El Mando del Rey de la Perdición', '14/11/2022', false, false, true, false),
+	('3375', 'Galaxy-Eyes', 'Galaxia XYZ', '14/11/2022', false, false, true, false),
+	('5862', 'Tech Genus', 'Control de Vuelo', '14/11/2022', false, false, true, false),
+	('6218', 'Galaxy-Eyes', 'Galaxia XYZ', '14/11/2022', false, false, true, false),
+	('2097', 'Fossil', 'Agujero de Gólem', '14/11/2022', false, false, true, false),
+	('3895', 'Tech Genus', 'Control de Vuelo', '14/11/2022', false, false, true, false),
+	('7217', 'Resonators', 'Resonancia Demoníaca', '14/11/2022', false, false, true, false),
+	('6715', 'D/D/D', 'El Mando del Rey de la Perdición', '14/11/2022', false, false, true, false),
+	('7989', 'Salamangreat', 'Vinculaciones', '14/11/2022', false, false, true, false),
+	('4691', 'D/D/D', 'El Mando del Rey de la Perdición', '14/11/2022', false, false, true, false),
+	('1475', 'Mermail Atlantean', 'Territorio de Tiburones', '14/11/2022', false, false, true, false),
+	('8099', 'Railway', 'Tren Imparable', '14/11/2022', false, false, true, false),
+	('1126', 'Galaxy-Eyes', 'Galaxia XYZ', '14/11/2022', false, false, true, false),
+	('1755', 'Tech Genus', 'Control de Vuelo', '14/11/2022', false, false, true, false),
+	('8712', 'Odd-Eyes', 'Xiangke y Xiangsheng', '14/11/2022', false, false, true, false),
+	('0074', 'Infernoid', 'Aumento de LP alfa', '14/11/2022', false, false, true, false),
+	('9682', 'Salamangreat', 'Alzarse desde el Valle de las Llamas', '15/11/2022', false, false, true, true),
+	('5069', 'Salamangreat', 'Alzarse desde el Valle de las Llamas', '15/11/2022', false, false, true, false),
+	('2047', 'Dark Magician', 'Robar sentido Luz', '15/11/2022', false, false, true, false),
+	('8797', 'Wounded Heros', 'Héroe Herido', '15/11/2022', false, false, true, false),
+	('8371', 'Block Dragon', 'Vinculaciones', '15/11/2022', false, false, true, false),
+	('5124', 'Mayosenju', 'Neo Nuevo Sylvio', '15/11/2022', false, false, true, false),
+	('1307', 'Burning Abyss', 'Mente de la Plana', '15/11/2022', false, false, true, false),
+	('3383', 'Salamangreat', 'Alzarse desde el Valle de las Llamas', '15/11/2022', false, false, true, false),
+	('8459', 'Salamangreat', 'Alzarse desde el Valle de las Llamas', '15/11/2022', false, false, true, true),
+	('7784', 'Wounded Heros', 'Héroe Herido', '15/11/2022', false, false, true, false),
+	('5740', 'Salamangreat', 'Guerrero de la Esperanza Número 39', '15/11/2022', false, false, true, false),
+	('1948', 'Dark Magician', 'As en la Manga', '16/11/2022', false, false, true, false),
+	('2077', 'Resonators', 'Resonancia Demoníaca', '16/11/2022', false, false, true, false),
+	('4446', 'Mekk-Knights', 'Mecanismos Antigüos', '16/11/2022', false, false, true, false),
+	('2432', 'Phantom Knights', 'La Orden de los Caballeros Fantasma', '16/11/2022', false, false, true, false),
+	('3417', 'Magnet Warrios', 'Robo del Destino', '16/11/2022', false, false, true, false),
+	('4158', 'Gandora', 'Luchar con Decisión', '16/11/2022', false, false, true, false),
+	('3267', 'Yosenjus', 'Robo del Destino', '16/11/2022', false, false, true, false),
+	('8496', 'Wounded Heros', 'Héroe Herido', '16/11/2022', false, false, true, false),
+	('6504', 'Meklord', 'Refinamiento Meklord', '16/11/2022', false, false, true, false),
+	('1546', 'Rose Dragon Mekk-Knights', 'Conjuro de Rosas', '14/11/2022', false, false, false, true),
+	('3209', 'Elemental Heros', 'Alianza de Héroes', '14/11/2022', false, false, false, true),
+	('1705', 'Mekk-Knights', 'Mecanismos Antigüos', '14/11/2022', false, false, false, true),
+	('4556', 'Odd-Eyes', 'Xiangke y Xiangsheng', '14/11/2022', false, false, false, true),
+	('0840', 'Salamangreat', 'Alzarse desde el Valle de las Llamas', '14/11/2022', false, false, false, true),
+	('9861', 'Infernoid', 'Con Valor', '15/11/2022', false, false, false, true),
+	('5679', 'Blackwings', 'Viento Negro hasta la Cima', '15/11/2022', false, false, false, true),
+	('6149', 'D/D/D', 'El Mando del Rey de la Perdición', '15/11/2022', false, false, false, true),
+	('4893', 'Salamangreat', 'Alzarse desde el Valle de las Llamas', '16/11/2022', false, false, false, true),
+	('3110', 'Abyss Actor', 'Tumbas Selladas', '16/11/2022', false, false, false, true);
+--
+-- Búsquedas de decks y skills
+select deck, skill from july22 where deck like '%x%';
+select deck, skill from ago22 where deck like '%x%';
+select deck, skill from ago22 where skill like '%volu%';
+select deck, skill from kcsep22 where skill like '%mp%';
+select deck, skill from kcsep22 where skill like '%nic%';
+select deck, skill from kcsep22 where skill like '%scur%';
+select deck, skill from kcsep22 where deck like '%rmo%';
+select deck, skill from kcsep22 where deck like '%ys%';
+select deck, skill from kcsep22 where deck like '%atel%';
+select deck, skill from kcsep22 where deck like '%uste%';
+select deck, skill from kcsep22 where deck like '%riam%';
+select deck, skill from kcsep22 where deck like '%luf%';
+select deck, skill from kcsep22 where deck like '%fer%';
+select deck, skill from kcsep22 where deck like '%ync%';
+select deck, skill from kcsep22 where deck like '%ando%';
+select deck, skill from kcsep22 where deck like '%ha%';
+select deck, skill from kcsep22 where deck like '%atel%';
+select deck, skill from kcsep22 where deck like '%oss%';
+select deck, skill from kcsep22 where deck like '%ech%';
+select deck, skill from kcsep22 where deck like '%enj%';
+select deck, skill from kcsep22 where deck like '%agne%';
+select deck, skill from kcnov22 where deck like '%ed-%';
+select deck, skill from kcnov22 where deck like '%ode %';
+select deck, skill from kcnov22 where deck like '%ailw%';
+select deck, skill from kcnov22 where deck like '%har%';
+select deck, skill from kcnov22 where deck like '%ala%';
+select deck, skill from kcnov22 where deck like '%dd-%';
+select deck, skill from kcnov22 where deck like '%imm%';
+select deck, skill from kcnov22 where deck like '%erm%';
+select deck, skill from kcnov22 where deck like '%dd-%';
+select deck, skill from kcnov22 where deck like '%xy-%';
+select deck, skill from kcnov22 where deck like '%ekk%';
+select deck, skill from kcnov22 where deck like '%osen%';
+select deck, skill from kcnov22 where deck like '%ha%';
+select deck, skill from kcnov22 where deck like '%ose D%';
+select deck, skill from kcnov22 where deck like '%/D%';
+select deck, skill from kcnov22 where deck like '%ekl%';
+select deck, skill from kcnov22 where deck like '%or%';
+select deck, skill from kcnov22 where deck like '%cto%';
+select deck, skill from kcnov22 where deck like '%ecr%';
+select deck, skill from kcnov22 where deck like '%ekk%';
+select deck, skill from kcnov22 where deck like '%ouk%';
+select deck, skill from kcnov22 where deck like '%ero%';
+select deck, skill from kcnov22 where deck like '%esona%';
+select deck, skill from kcnov22 where deck like '%elo%';
+select deck, skill from kcnov22 where deck like '%urni%';
+select deck, skill from kcnov22 where deck like '%ekk%';
+select deck, skill from kcnov22 where skill like '%LP%';
+select deck, skill from kcnov22 where deck like '%lack%';
+--
+select * from kcnov22 where usuario like '%2941%';
+update kcnov22 set bryan = true where id = 148;
+update kcnov22 set xenoblur = true where id = 148;
+select * from kcnov22 where usuario like '%1426%';
+update kcnov22 set xenoblur = true where id = 37;
+select * from kcnov22 where usuario like '%5335%';
+update kcnov22 set xenoblur = true where id = 131;
+--
+select deck, skill, ndmax from kcnov22 order by id;
+select deck, skill, ndmax, zerotg, zephra, bryan, xenoblur from kcnov22 order by ndmax;
+--
